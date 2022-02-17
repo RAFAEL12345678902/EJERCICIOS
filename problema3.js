7403eaa8418952170db3a612a997053e4b3e20e2
RAFAEL12345678902 <76074240+RAFAEL12345678902@users.noreply.github.com>
2022-02-17 02:37:01 -0600

    Create problema3.js

---
+++ problema3.js
@@ -0,0 +1,10 @@
+/*Haz un javascript para: Calcular el área de un triángulo. ¿Qué variables necesitas? Los valores deben de ser capturados con promt y mostrados con alertas.*/
+
+
+let base =prompt("Ingresa la base del triangulo");
+let altura =prompt("Ingresa la altura del triangulo");
+let area;
+
+area = (base*altura) /2;
+
+alert("El area del triangulo es: " + area +"cm²");
