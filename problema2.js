6667a9d3b7c9503975ae05303712e86e344926ca
RAFAEL12345678902 <76074240+RAFAEL12345678902@users.noreply.github.com>
2022-02-17 02:36:34 -0600

    Create problema2.js

---
+++ problema2.js
@@ -0,0 +1,18 @@
+/*Escribe la variable para:
+Guardar tu edad
+Guardar tu nombre
+Guardar tu dirección.
+Guardar el precio de un bolillo
+Guardar la fecha de nacimiento
+Guardar tu clave de elector
+Guardar el nombre de tu mascota*/
+
+let edad ="Tengo 24 años," ;
+let nombre =" me llamos Rafael";
+let dirección =" vivo en CDMX, TLAHUAC,";
+let precio =" El bolillo cuesta 2 pesos";
+let fecha =" Naci el 3 De novimbre de 1997";
+let ine = " Mi clave de elector es: 1234567";
+let mascota ="Mi mascota se llama LOCO";
+alert("Tus datos son: "+ edad +" " + nombre + " " + dirección + " " + precio + " " + fecha + " " + ine + " " + mascota );
+
