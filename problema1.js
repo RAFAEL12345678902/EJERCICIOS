90020248ebb3f1c4140e173cbd94eab05b1ed9fc
RAFAEL12345678902 <76074240+RAFAEL12345678902@users.noreply.github.com>
2022-02-17 02:35:57 -0600

    Create problema1.js

---
+++ problema1.js
@@ -0,0 +1,12 @@
+/*Declara la variable suma
+Declara  una variable llamada x y pídela al usuario.
+Declara  una variable llamada y  pídela al usuario.
+Asigna el resultado de x más y.
+Imprime en consola “La suma es: ” seguida del valor de la variable suma.*/
+
+
+
+let x = prompt("DAME X");
+let y = prompt("DAME Y");
+let suma = parseFloat(x) + parseFloat(y);
+alert("La suma es: " +suma);
