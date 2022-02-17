c84e5277ca6c570aec37d3751a2164330206cc96
RAFAEL12345678902 <76074240+RAFAEL12345678902@users.noreply.github.com>
2022-02-17 02:38:01 -0600

    Create problema5.js

---
+++ problema5.js
@@ -0,0 +1,13 @@
+/*Te han contratado en una empresa de venta de ropa. Se llegó el buen fin y una de tus tareas es que calcules 
+ ¿Cuál tendría que ser el precio final de un saco super padrísimo que cuesta 1560 que tiene un descuento de 18%?
+¿Cuáles tendrían que ser las modificaciones que tendrías que hacer en caso de que el descuento sea 10%.*/
+
+
+let  calcularPorcentaje= 1560;
+let descuento ;
+descuento = calcularPorcentaje *.18;
+let descuento_2;
+descuento_2= calcularPorcentaje*.10;
+
+alert("El Primer descuento que da la tienda es de 18%:  " +descuento +" "+ "Pero si es VIP la tienda le da el descuento de 10% que es: "+ descuento_2);
+
