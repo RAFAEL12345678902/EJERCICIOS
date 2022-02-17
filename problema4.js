a994c1bcfb44149d57f95012516890933c135290
RAFAEL12345678902 <76074240+RAFAEL12345678902@users.noreply.github.com>
2022-02-17 02:37:25 -0600

    Create problema4.js

---
+++ problema4.js
@@ -0,0 +1,10 @@
+/*Haz javascript para : Calcular el perímetro de un cuadrado. Los valores deben de ser capturados con promt y mostrados con alertas.*/
+
+
+let lado1 =prompt("Ingresa lado 1 cuadrado");
+let lado2 =prompt("Ingresa lado 2  cuadrado");
+let lado3 =prompt("Ingresa lado 3  cuadrado");
+let lado4 =prompt("Ingresa lado 4  cuadrado");
+let perimetro;
+perimetro = parseFloat(lado1) + parseFloat(lado2)+ parseFloat(lado3) + parseFloat(lado4);
+alert("El area del  cuadrador  es: " + perimetro );
